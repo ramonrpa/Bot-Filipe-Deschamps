19c452ffc6a28f9fe99ea45e91e42041bcc5bfba
Tsugami <yslan3000@gmail.com>
2019-03-06 13:09:25 -0300

    avatar command adapted

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,86 +1,38 @@
-module.exports = {
-    run: (client, message, args) => {
-
-        if (!message.member) return
-        let user;
-
-        if (message.mentions.users.first()) {
-            user = message.mentions.users.first();
-        } else {
-            user = message.author;
-        }
-        const m = message.channel.send("Avatar");
-
-        const member = message.guild.member(user);
-        let embed = {
-            "color": 15614245,
-            "title": `${user.username}#${user.discriminator}`,
-            "thumbnail": {
-                "url": user.avatarURL
-            },
-            "fields": [{
-                    "name": "ID:",
-                    "value": `${user.id}`,
-                    "inline": true
-                },
-                {
-                    "name": "Nickname:",
-                    "value": `${member.nickname}` !== null ? `${user.username}` : 'None',
-                    "inline": true
-                },
-                {
-                    "name": "Created At:",
-                    "value": `${formatDate('DD/MM/YYYY, às HH:mm:ss', user.createdAt)}`,
-                    "inline": true
-                },
-                {
-                    "name": "Joined Server:",
-                    "value": `${formatDate('DD/MM/YYYY, às HH:mm:ss',user.joinedAt)}`,
-                    "inline": true
-                },
-                {
-                    "name": "Bot:",
-                    "value": `${user.bot}`,
-                    "inline": true
-                },
-                {
-                    "name": "Status:",
-                    "value": `${user.presence.status}`,
-                    "inline": true
-                },{
-                    "name": "Game:",
-                    "value": `${user.presence.game ? user.presence.game.name : 'None'}`,
-                    "inline": true
-                },{
-                    "name": "Roles:",
-                    "value": member.roles.map(roles => `${roles.name}`).join(', '),
-                    "inline": true
-                },
-            ]
-        }
-        message.channel.send({
-            embed
-        })
-    },
-
-    conf: {
-        onlyguilds: true
-    },
-
-    get help() {
-        return {
-            name: 'avatar',
-            category: 'Moderação',
-            description: 'Retornar info do membro.',
-            usage: 'avatar'
-        }
-    }
-}
+const Command = require('../strucutres/Command')
+const { RichEmbed } = require('discord.js')
 
 function formatDate(template, date) {
-    var specs = 'YYYY:MM:DD:HH:mm:ss'.split(':')
+    const specs = 'YYYY:MM:DD:HH:mm:ss'.split(':')
     date = new Date(date || Date.now() - new Date().getTimezoneOffset() * 6e4)
     return date.toISOString().split(/[-:.TZ]/).reduce(function (template, item, i) {
         return template.split(specs[i]).join(item)
     }, template)
 }
+
+class Avatar extends Command {
+  constructor (client) {
+    super(client)
+    this.guildOnly = true
+    this.category = 'Moderação'
+    this.description = 'Retornar info do membro.'
+  }
+
+  run (message) {
+    const member = message.mentions.members.first() || message.member
+    const embed = new RichEmbed()
+        .setColor(15614245)
+        .setTitle(member.user.tag)
+        .setThumbnail(member.user.displayAvatarURL)
+        .addField('ID:', member.user.id, true)
+        .addField('Nickname:', member.nickname || 'None', true)
+        .addField('Created At:', formatDate('DD/MM/YYYY, às HH:mm:ss', member.user.createdAt), true)
+        .addField('Joined At:', formatDate('DD/MM/YYYY, às HH:mm:ss', member.joinedAt), true)
+        .addField('Bot:', `${member.user.bot}`, true)
+        .addField('Status:', member.user.presence.status, true)
+        .addField('Game:', (member.user.presence.game && member.user.presence.game.name) || 'None', true)
+        .addField('Roles:', member.roles.map(role => role.name).join(', '))
+    message.channel.send(embed)
+  }
+}
+
+module.exports = Avatar
