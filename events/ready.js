ee51dc9c4804109db052903c9361413654f1db40
Ramon Rodrigues <47230851+ramonrpa@users.noreply.github.com>
2019-03-09 11:11:32 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 
 module.exports = function onReady() {
   console.log('log', `O Bot foi iniciado completamente com ${this.users.size} usuarios em ${this.guilds.size} servidores`)
-  this.user.setPresence({ status: 'online', game: { name: process.env.GAME } })
+  this.user.setPresence({ status: 'online', game: { name: process.env.STATUS } })
 }
